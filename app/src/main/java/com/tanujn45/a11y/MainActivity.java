405a90ea5db6031249c475c350226ea4f89ef1c5
tanujn45 <tanujn45@gmail.com>
2023-11-08 11:32:59 -0500

    Initial commit

---
+++ app/src/main/java/com/tanujn45/a11y/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tanujn45.a11y;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

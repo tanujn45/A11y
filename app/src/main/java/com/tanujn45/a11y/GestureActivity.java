b1cddbe825a8fe4a49e6f9c69cdeadd4b1457a75
tanujn45 <tanujn45@gmail.com>
2023-12-07 18:52:42 -0500

    1. Implemented the sliding window for gesture recognition. 2. Need to fix the problems with File package in Java

--- app/src/main/java/com/tanujn45/a11y/GestureActivity.java
+++ app/src/main/java/com/tanujn45/a11y/GestureActivity.java
@@ -3,6 +3,7 @@ package com.tanujn45.a11y;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +38,8 @@ public class GestureActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private CardAdapter adapter;
     private MdsSubscription mdsSubscription;
+    private double currentTimeSeconds = System.currentTimeMillis() / 1000.0;
+    private double previousTime = System.currentTimeMillis() / 1000.0;
 
     private Mds getMds() {
         return MainActivity.mMds;
@@ -54,7 +57,6 @@ public class GestureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture);
-
 
         connectedSerial = getConnectedSerial();
 
@@ -129,6 +131,10 @@ public class GestureActivity extends AppCompatActivity {
         String strContract = "{\"Uri\": \"" + connectedSerial + PATH + RATE + "\"}";
         Log.d(LOG_TAG, strContract);
 
+        List<Float> accX = new ArrayList<>();
+        List<Float> accY = new ArrayList<>();
+        List<Float> accZ = new ArrayList<>();
+
         mdsSubscription = getMds().builder().build(this).subscribe(URI_EVENTLISTENER, strContract, new MdsNotificationListener() {
             @Override
             public void onNotification(String data) {
@@ -139,6 +145,25 @@ public class GestureActivity extends AppCompatActivity {
                 if (imuModel != null && imuModel.getBody().getArrayAcc().length > 0 && imuModel.getBody().getArrayGyro().length > 0) {
 
                     String resultStr = String.format(Locale.getDefault(), "%.2f %.2f %.2f\n%.2f %.2f %.2f", imuModel.getBody().getArrayAcc()[0].getX(), imuModel.getBody().getArrayAcc()[0].getY(), imuModel.getBody().getArrayAcc()[0].getZ(), imuModel.getBody().getArrayGyro()[0].getX(), imuModel.getBody().getArrayGyro()[0].getY(), imuModel.getBody().getArrayGyro()[0].getZ());
+
+                    currentTimeSeconds = System.currentTimeMillis() / 1000.0;
+                    accX.add((float) imuModel.getBody().getArrayAcc()[0].getX());
+                    accY.add((float) imuModel.getBody().getArrayAcc()[0].getY());
+                    accZ.add((float) imuModel.getBody().getArrayAcc()[0].getZ());
+
+                    double elapsedTime = currentTimeSeconds - previousTime;
+                    if (elapsedTime >= 1.0) {
+                        float[][] acc = new float[3][accX.size()];
+
+
+                        for (int i = 0; i < accX.size(); i++) {
+                            acc[0][i] = accX.get(i);
+                            acc[1][i] = accY.get(i);
+                            acc[2][i] = accZ.get(i);
+                        }
+                        checkForGesture(acc);
+                        previousTime = currentTimeSeconds;
+                    }
 
                     sensorMsg.setText(resultStr);
                 }
@@ -151,6 +176,71 @@ public class GestureActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void checkForGesture(float[][] acc) {
+        String circleClockwise = "";
+        String circleAnticlockwise = "";
+
+        double clockwiseDistance = getAvgDistance(circleClockwise, acc);
+        double anticlockwiseDistance = getAvgDistance(circleAnticlockwise, acc);
+
+
+        if (true) {
+            String textToSpeak = "Gesture detected!";
+            tts.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+    }
+
+    public double getAvgDistance(String fileToCompare, float[][] dataRecorded) {
+        double avgDistance = 0;
+
+        for (int i = 1; i < 4; i++) {
+            DTW dtw = new DTW();
+
+            DTW.Result result = dtw.compute(getDatafromCSV(fileToCompare, i), dataRecorded[i - 1]);
+
+            int[][] warpingPath = result.getWarpingPath();
+            double distance = result.getDistance();
+
+            avgDistance += (distance * distance);
+        }
+
+        return avgDistance;
+    }
+
+    public float[] getDatafromCSV(String fileName, int dataType) {
+        List<Float> accL = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            boolean skipFirstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (skipFirstLine) {
+                    skipFirstLine = false;
+                    continue;
+                }
+                String[] data = line.split(",");
+                if (data.length >= 7) {
+                    try {
+                        accL.add(Float.parseFloat(data[dataType]));
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        float[] acc = new float[accL.size()];
+
+        for (int i = 0; i < accL.size(); i++) {
+            acc[i] = accL.get(i);
+        }
+
+        return acc;
+    }
+
 
     private void unsubscribe() {
         if (mdsSubscription != null) {
